39f8adc240cc7e2bd34316d9a303b8e4aed66db1
krysqk <60812838+krysqk@users.noreply.github.com>
2020-02-16 10:23:32 +0100

    Klasa sprawdzająca pełnoletnosć

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Main {
-    
-}
+    public static void main(String[] args) {
+        System.out.println("Urodzony 17.02.2009");
+        if (majority(17,2,2009))
+        System.out.println("pełnoletni");
+        else
+        System.out.println("niepełnoletni");
+    }//end main
+    public static boolean majority(int day, int month, int year){
+        LocalDate dateOfBrith, now;
+        dateOfBrith = LocalDate.of(year,month,day);
+        now = LocalDate.now();
+        Period p = Period.between(dateOfBrith,now);
+        if(p.getYears()>=18) return true;
+        return false;
+
+    }
+}//end class
